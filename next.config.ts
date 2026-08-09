9c6792bfdbe826619a233c06e86c0cdeefb7a8d3
ronell-agustin <ronellagustin@outlook.com>
2026-08-10 00:46:53 +0800

    Allow optimized YouTube images

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
