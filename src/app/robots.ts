322b7144085103bbe92741ed33a93bdb34cb4803
ronell-agustin <ronellagustin@outlook.com>
2026-08-10 19:33:04 +0800

    Align robots sitemap with www production domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://ronellagustin.com/sitemap.xml",
+    sitemap: "https://www.ronellagustin.com/sitemap.xml",
   };
 }
