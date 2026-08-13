3b87ad5d5455236a735a20df7bbb986649b06a35
ronell-agustin <ronellagustin@outlook.com>
2026-08-13 09:29:54 +0800

    Enable Vercel Analytics in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import ScrollToTop from "./ScrollToTop";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
-
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.ronellagustin.com"),
@@ -57,6 +56,7 @@ export default function RootLayout({
       <body>
         {children}
         <ScrollToTop />
+        <Analytics />
       </body>
     </html>
   );
